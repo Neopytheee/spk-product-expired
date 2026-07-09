ced5f6a9726aa6d7632dbf581eec4f10c00eaeb4
Aspshrdn <119942131+Neopytheee@users.noreply.github.com>
2026-07-09 15:05:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 
 # ==============================================================================
 # IMPORT DATASET
